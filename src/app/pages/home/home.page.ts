f4f06355ec589d4ad58a631545e38ae0c50b21dc
Erik Tomisaki Rodrigues da Silva <erik.trs01@gmail.com>
2025-06-17 21:40:39 -0300

    Added functionality to improve Pokémon display and navigation on Home Page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import {
   IonHeader,
@@ -11,6 +12,7 @@ import {
   IonRow,
   IonCol,
   IonButtons,
+  IonButton,
   IonCard,
   IonCardContent,
   IonCardHeader,
@@ -24,6 +26,7 @@ import { PokemonService } from '../../services/pokemon.service';
   imports: [
     CommonModule,
     IonButtons,
+    IonButton,
     IonMenuButton,
     IonHeader,
     IonToolbar,
@@ -36,28 +39,36 @@ import { PokemonService } from '../../services/pokemon.service';
     IonCardHeader,
     IonCardTitle,
     IonCardContent
-  ],
+],
 })
 
 export class HomePage implements OnInit {
   pokemons: any[] = [];
 
-  constructor(private pokemonService: PokemonService) {}
+  constructor(private pokemonService: PokemonService, private router: Router) {}
 
   ngOnInit() {
+    // Load the first 20 Pokémons when the component initializes
     this.loadPokemons(20, 0);
   }
 
+  // Method to load Pokémons with pagination
   loadPokemons(limit: number, offset: number) {
     this.pokemonService.getPokemonList(limit, offset).subscribe((response: any) => {
-      this.pokemons = response.results.map((p: any) => ({
-        name: p.name,
-        url: p.url,
-        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.extractId(p.url)}.png`
+      this.pokemons = response.results.map((pokemon: any) => ({
+        name: pokemon.name,
+        url: pokemon.url,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.extractId(pokemon.url)}.png`
       }));
     });
   }
 
+  // Method to navigate to the details page with the Pokémon name
+  viewDetails(pokemon: any) {
+    this.router.navigate(['details', pokemon]);
+  }
+
+  // Private method to extract the Pokémon ID from the URL
   private extractId(url: string): string {
     const parts = url.split('/');
     return parts[parts.length - 2];
