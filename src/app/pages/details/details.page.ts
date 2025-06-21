a6c6e6fa2bc1771746345f5fba9bca27498021ac
Erik Tomisaki Rodrigues da Silva <erik.trs01@gmail.com>
2025-06-20 22:39:09 -0300

    Added related images and evolutions in Details page

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -13,6 +13,10 @@ import {
   IonCardContent,
   IonButton,
   IonSpinner,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonText,
 } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -33,10 +37,10 @@ import { FormsModule } from '@angular/forms';
     IonTitle,
     IonToolbar,
     IonHeader,
-    IonContent,
-    IonHeader,
-    IonTitle,
-    IonToolbar,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonText,
     CommonModule,
     FormsModule,
   ],
@@ -44,6 +48,8 @@ import { FormsModule } from '@angular/forms';
 export class DetailsPage implements OnInit {
   pokemon: any;
   loading = true;
+  evolutions: { name: string; image: string }[] = [];
+  relatedImages: string[] = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -72,6 +78,13 @@ export class DetailsPage implements OnInit {
           weight: data.weight,
           isFavorite: this.favoriteService.isFavorite(data.id),
         };
+
+        // Related images: add other sprite versions
+        this.relatedImages = this.getRelatedSprites(data.sprites);
+
+        // Load species → evolution chain
+        this.loadEvolutions(data.id);
+
         this.loading = false;
       },
       error: (err) => {
@@ -79,6 +92,53 @@ export class DetailsPage implements OnInit {
         this.loading = false;
       },
     });
+  }
+
+  getRelatedSprites(sprites: any): string[] {
+    const images: string[] = [];
+    if (sprites.front_shiny) images.push(sprites.front_shiny);
+    if (sprites.back_default) images.push(sprites.back_default);
+    if (sprites.back_shiny) images.push(sprites.back_shiny);
+    if (sprites.other?.dream_world?.front_default)
+      images.push(sprites.other.dream_world.front_default);
+    return images;
+  }
+
+  loadEvolutions(idOrName: string) {
+    this.pokemonService.getPokemonSpecies(idOrName).subscribe({
+      next: (speciesData) => {
+        const evoUrl = speciesData.evolution_chain.url;
+        this.pokemonService.getEvolutionChainByUrl(evoUrl).subscribe({
+          next: (evoData) => {
+            this.evolutions = this.extractEvolutions(evoData.chain);
+          },
+          error: (err) => console.error('Error loading evolution chain:', err),
+        });
+      },
+      error: (err) => console.error('Error loading species data:', err),
+    });
+  }
+
+  extractEvolutions(chain: any): { name: string; image: string }[] {
+    const evolutions: { name: string; image: string }[] = [];
+
+    const traverse = (node: any) => {
+      const name = node.species.name;
+      const id = this.pokemonService.getPokemonIdByName(name);
+
+      const imageUrl = id
+        ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+        : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.pokemonService.extractId(
+            node.species.url
+          )}.png`;
+
+      evolutions.push({ name, image: imageUrl });
+
+      node.evolves_to.forEach((evo: any) => traverse(evo));
+    };
+
+    traverse(chain);
+    return evolutions;
   }
 
   toggleFavorite() {
